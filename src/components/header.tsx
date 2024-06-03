96c57f4e3b1c66ac84df67637f149285b6fa6183
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-06-03 20:40:50 -0300

    chore: :lipstick: style adjustments on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ import logo from "/coffee-delivery-logo.svg";
 
 export function Header() {
   return (
-    <header className="py-8 flex justify-between items-center">
+    <header className="py-8 w-[70rem] px-4 mx-auto flex justify-between items-center">
       <img src={logo} />
 
       <div className="flex gap-3">
-        <div className="p-2 flex gap-1 items-center bg-purple-200 rounded-lg">
+        <div className="p-2 flex gap-1 items-center bg-purple-200 rounded-lg ">
           <FaLocationDot className="text-purple-500" />
-          <p>Lages, SC</p>
+          <p className="text-purple-700">Lages, SC</p>
         </div>
 
         <button className="py-2 px-3 bg-yellow-100 rounded-lg">
