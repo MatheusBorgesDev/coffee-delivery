47911e5e243577e8d87d7fc40e4e7752a13fca9b
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-06-17 18:25:07 -0300

    feat: :sparkles: add function to increase and decrease coffee quantity on coffee card component

--- src/components/coffeeCard.tsx
+++ src/components/coffeeCard.tsx
@@ -1,26 +1,32 @@
+import { useState } from "react";
+
 import {
   PiMinusBold,
   PiPlusBold,
   PiShoppingCartSimpleFill,
 } from "react-icons/pi";
 
-interface CoffeeCardProps {
-  imgUrl: string;
-  title: string;
-  tags: string[];
-  description: string;
-}
+import { Coffee } from "../constants/coffeesData";
 
-export function CoffeeCard({
-  imgUrl,
-  tags,
-  title,
-  description,
-}: CoffeeCardProps) {
+export function CoffeeCard({ id, tags, title, description, price }: Coffee) {
+  const [quantity, SetQuantity] = useState(1);
+
+  function handleIncreaseQuantity() {
+    SetQuantity(quantity + 1);
+  }
+
+  function handleDecreaseQuantity() {
+    if (quantity == 1) {
+      return;
+    } else {
+      SetQuantity(quantity - 1);
+    }
+  }
+
   return (
     <div className="bg-zinc-200/40 px-6 py-5 rounded-lg rounded-tr-[2.5rem] rounded-bl-[2.5rem]">
       <div className="flex flex-col gap-3 items-center text-center">
-        <img src={imgUrl} alt="" className="w-[7.5rem] -mt-10" />
+        <img src={id} alt="" className="w-[7.5rem] -mt-10" />
 
         <div className="flex gap-1">
           {tags.map((tag) => (
@@ -41,18 +47,25 @@ export function CoffeeCard({
 
       <div className="mt-8 flex items-center justify-between ">
         <p className="text-sm opacity-70">
-          R$ <span className="text-2xl font-baloo font-extrabold ">9,90</span>
+          R${" "}
+          <span className="text-2xl font-baloo font-extrabold ">{price}</span>
         </p>
 
         <div className="flex gap-2 h-9">
           <div className="p-2 flex items-center gap-2 bg-zinc-200 rounded-md">
-            <button className="text-purple-700">
+            <button
+              onClick={handleDecreaseQuantity}
+              className="text-purple-700"
+            >
               <PiMinusBold />
             </button>
 
-            <span>1</span>
+            <span>{quantity}</span>
 
-            <button className="text-purple-700">
+            <button
+              onClick={handleIncreaseQuantity}
+              className="text-purple-700"
+            >
               <PiPlusBold />
             </button>
           </div>
