9fc12ee0487a37dad6ee112dfb37f094c556427f
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-06-17 22:23:49 -0300

    feat: :sparkles: add cart context to show number of products on cart

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+
 import { FaLocationDot } from "react-icons/fa6";
 import { PiShoppingCartFill } from "react-icons/pi";
+
+import { CartContext } from "../contexts/cartContext";
 
 import logo from "/coffee-delivery-logo.svg";
 
 export function Header() {
+  const { coffeesAmount } = useContext(CartContext);
+
   return (
     <header className="py-8 w-[70rem] px-4 mx-auto flex justify-between items-center">
       <img src={logo} />
@@ -14,8 +20,13 @@ export function Header() {
           <p className="text-purple-700">Lages, SC</p>
         </div>
 
-        <button className="p-2 bg-yellow-200 rounded-lg">
+        <button className="p-2 bg-yellow-200 rounded-lg relative">
           <PiShoppingCartFill size={20} className="text-yellow-600" />
+          {coffeesAmount !== 0 && (
+            <div className="rounded-full bg-yellow-700 absolute top-[-7px] right-[-10px] text-white px-[6px] text-sm">
+              {coffeesAmount}
+            </div>
+          )}
         </button>
       </div>
     </header>
