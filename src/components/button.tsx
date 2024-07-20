613ae8b3cd84492e6f295c1013a3cb2ceb57aac9
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-07-20 12:31:21 -0300

    refactor: :lipstick: change button style when selected

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,22 @@
 import { ButtonHTMLAttributes } from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  selected?: boolean;
+}
 
-export function Button({ className, children, ...props }: ButtonProps) {
+export function Button({
+  className,
+  children,
+  selected,
+  ...props
+}: ButtonProps) {
   return (
     <button
-      className={`p-4 w-full flex items-center gap-3 bg-zinc-200 text-xs uppercase rounded-md hover:bg-purple-200/50 transition border border-transparent focus-within:bg-purple-200/50 focus-within:border-purple-600
-        ${className}`}
+      className={`p-4 w-full flex items-center gap-3 rounded-md text-xs uppercase transition border ${
+        selected
+          ? "bg-purple-200/50 border-purple-600 text-purple-600"
+          : "bg-zinc-200 text-zinc-600 border-transparent hover:bg-purple-200/50"
+      } ${className}`}
       {...props}
     >
       {children}
