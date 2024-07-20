fc3ffb7cc7bb01c95c6139c106768577e31c89f3
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-07-20 12:32:04 -0300

    feat: :sparkles: add validation error message

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,12 +1,30 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, forwardRef } from "react";
+import { FieldError } from "react-hook-form";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
-
-export function Input({ className, ...props }: InputProps) {
-  return (
-    <input
-      className={`px-3 py-2 bg-zinc-200/30 rounded border border-zinc-300/50 placeholder:text-zinc-500/80 focus:outline focus:outline-purple-600 ${className}`}
-      {...props}
-    />
-  );
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: string | FieldError;
 }
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, error, ...props }, ref) => {
+    const isError = !!error;
+    const errorMessage = typeof error === "string" ? error : error?.message;
+
+    return (
+      <div className="flex flex-col">
+        <input
+          ref={ref}
+          className={`px-3 py-2 bg-zinc-200/30 rounded border ${
+            isError ? "border-red-500" : "border-zinc-300/50"
+          } placeholder:text-zinc-500/80 focus:outline focus:outline-purple-600 ${className}`}
+          {...props}
+        />
+        {isError && (
+          <span className="text-red-500 text-sm mt-1">{errorMessage}</span>
+        )}
+      </div>
+    );
+  }
+);
+
+export { Input };
